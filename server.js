097de22714b78aed55bfa6b24ab13f5164b18b71
isfandiyor <isfandiyor.vohidovv@mail.ru>
2025-07-23 18:03:07 +0500

    Commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const corsOptions = {
     origin: 'http://127.0.0.1:5501',
 };
